69fa2dcba6b59618cccbc64b20aa9bb2008385ef
Shuichi Nagao <ngo38114@gmail.com>
2017-01-15 09:09:06 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render text: "hello, world!"
+        #require 'yelp'
+
+    client = Yelp::Client.new({ 
+      consumer_key: "XXXXXXXX",
+      consumer_secret: "XXXXXXXX",
+      token: "XXXXXXXX",
+      token_secret: "XXXXXXXXX"
+    })
+
+    #render text: "hello, world!"
+    params = {
+      term: 'food',
+      limit: 3
+#      category_filter: 'discgolf'
+    }
+    #locale = { lang: 'fr' }
+
+    result =  client.search('San Francisco', params)
+
+    print(result.businesses)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,13 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    Yelp.client.configure do |config|
+      config.consumer_key = 'MIbzSvueKO0xyPYZuxkD-g'
+      config.consumer_secret = 'TzxdmAnrJ1m40T34-0n8MSpBHnE'
+      config.token = 'm6GeEZ_h2IyueBNVkvFllHe67YWxKgUW'
+      config.token_secret = 'qyIzFmlESlYqCVLFDTpEzRfauoA'
+    end
+   # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
