f0628ff31c36d3d3408b743019bf6d202818f932
Shuichi Nagao <ngo38114@gmail.com>
2017-01-23 22:45:05 +0900

    fix bugs in index page

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,6 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    Yelp.client.configure do |config|
-      config.consumer_key = 'MIbzSvueKO0xyPYZuxkD-g'
-      config.consumer_secret = 'TzxdmAnrJ1m40T34-0n8MSpBHnE'
-      config.token = 'm6GeEZ_h2IyueBNVkvFllHe67YWxKgUW'
-      config.token_secret = 'qyIzFmlESlYqCVLFDTpEzRfauoA'
-    end
    # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
