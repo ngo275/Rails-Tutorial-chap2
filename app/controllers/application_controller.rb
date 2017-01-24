dc2b431676fba36c69dc67f35225a50967d29c23
Shuichi Nagao <ngo38114@gmail.com>
2017-01-24 09:19:23 +0900

    finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
+#  def hello
         #require 'yelp'
-    print("hello")
-  end
+#    print("hello")
+#  end
 end
